b854734523ba13d40b4177889f484e3bd69d3b8d
ronisgracie <ronis@gracieoc.com>
2016-03-02 13:45:16 -0800

    create app homesite and view homesite

--- graciefitapi/urls.py
+++ graciefitapi/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from homesite.views import HomesiteView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomesiteView.as_view())
 ]
