147455ce147c7ab048639789477bf48ecdac4a63
Leandro Barbosa <leandrobar93@gmail.com>
2016-03-03 15:09:22 -0300

    update project and gitignore

--- graciefitapi/urls.py
+++ graciefitapi/urls.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from homesite.views import HomesiteView
 
@@ -9,4 +11,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', HomesiteView.as_view()),
     url(r'^api/v1/', include('api.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
