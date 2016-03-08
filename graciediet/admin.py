684c653f8edb2158779198d38ae62229afe5f4b1
Leandro Barbosa <leandrobar93@gmail.com>
2016-03-08 19:50:56 -0300

    update app

--- graciediet/admin.py
+++ graciediet/admin.py
@@ -1,3 +1,9 @@
+# coding: utf-8
 from django.contrib import admin
 
-# Register your models here.
+from .models import GroupFood, TypeFood, Food
+
+
+admin.site.register(GroupFood)
+admin.site.register(TypeFood)
+admin.site.register(Food)
